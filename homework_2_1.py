806321cc31dda8e175b3218b7d1e6b5437c662bd
andrewandrewsfromrussia <160389234+andrewandrewsfromrussia@users.noreply.github.com>
2024-02-19 18:54:48 +0500

    Add files via upload

---
+++ homework_2_1.py
@@ -0,0 +1,3 @@
+height, width = map(int, input('Введите высоту и ширину: ').split())
+print('Площадь прямоугольника равна', height * width)
+print('Периметр прямоугольника равен', (height + width) * 2)
