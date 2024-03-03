07aeb2033feaf97d2cf1dbcc2ddbba36287bedc7
andrewandrewsfromrussia <160389234+andrewandrewsfromrussia@users.noreply.github.com>
2024-03-03 13:00:13 +0500

    Update homework_4_3.py

--- homework_4_3.py
+++ homework_4_3.py
@@ -1,5 +1,8 @@
 total = 0
+even = []
 for i in range(int(input('Введите первое число: ')), int(input('Введите второе число, больше первого: ')) + 1):
     if i % 2 == 0:
         total += 1
+        even.append(i)
 print('На заданном отрезке', total, 'четных чисел.')
+print('Вот эти числа:', *even)
